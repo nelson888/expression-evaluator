1eec5e677617dd62423fce7bb4036072259ffbdf
nelson888 <tambue@hotmail.fr>
2019-06-19 07:43:57 +0200

    implemented for additions/soustractions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::collections::HashMap;
 use std::process::exit;
 
 type Integer = u32;
@@ -8,7 +7,7 @@ trait Computable {
     fn compute(&self) -> Integer {
         return 0;
     }
-    fn operator_compute(&self, arg1 : &Computable, arg2: &Computable) -> Integer {
+    fn operator_compute(&self, _arg1 : &Computable, _arg2: &Computable) -> Integer {
         return 0;
     }
     fn is_operator(&self) -> bool;
@@ -19,9 +18,24 @@ type Operation = Vec<Box<Computable>>;
 struct Constant {
     value : Integer
 }
+impl Constant {
+    fn of(value : Integer) -> Constant {
+        return Constant {
+            value
+        };
+    }
+}
 
-struct  Operator {
+struct Operator {
     symbol: char
+}
+
+impl Operator {
+    fn of(symbol : char) -> Operator {
+        return Operator {
+            symbol
+        };
+    }
 }
 
 impl Computable for Constant {
@@ -68,29 +82,34 @@ fn get_first_char(s: &String) -> char {
 fn to_computable(s: String) -> Box<Computable> {
     let first_char: char = get_first_char(&s);
     if first_char.is_ascii_digit() {
-        return Box::new(Constant {
-            value: to_int(&s)
-        });
+        return Box::new(Constant::of(to_int(&s)));
     } else {
-        return Box::new(Operator {
-            symbol: first_char
-        });
+        return Box::new(Operator::of(first_char));
     }
 }
 //step 1: ^, step 2: * et /, step 3: + et -
-fn simplify(op : Operation, step : u8) -> Operation {
-    let mut new_op : Operation = Vec::new();
-    /*
-    done : bool = false;
-    while !done {
-        for (i, item) in new_op.iter().enumerate() {
-            println!("The {}th item is {}", i+1, item);
+fn simplify(mut op : Operation, step : u8) -> Operation {
+    loop {
+        let opt_op_pos: Option<usize> = op.iter().position(|c| c.is_operator()); //TODO change function is_operator to check if specific operator given the step
+        if opt_op_pos.is_none() {
+            break;
         }
-    }*/
-    new_op.push(Box::new(Constant {
-        value: 8
-    }));
-    return new_op;
+        //get the operator and compute
+        let operator_pos: usize = opt_op_pos.unwrap();
+        let result = Box::new(
+            Constant::of(
+                op[operator_pos].as_ref()
+                    .operator_compute(op[operator_pos - 1].as_ref(),
+                                      op[operator_pos + 1].as_ref())
+        ));
+
+        //replace the operator and its parameter to the result
+        op.remove(operator_pos);
+        op.insert(operator_pos, result);
+        op.remove(operator_pos + 1);
+        op.remove(operator_pos - 1);
+    }
+    return op;
 }
 //a=4, b=5; a + a * b
 fn main() {
